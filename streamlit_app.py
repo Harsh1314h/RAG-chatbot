41b13eccb18241a80d3545746d0c38dada72840c
Harsh1314h <harsh@fix.com>
2026-05-18 20:12:01 +0530

    fix: protobuf/chromadb Python 3.13 compatibility on Streamlit Cloud

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
+import os
+os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
+
 import streamlit as st
 from src.llm_wrapper import stream_generate
 from src.retrieval import Retriever
